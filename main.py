5b58adba1cef0c48f84c40f8311c8e2a828e401f
azerowall <azerowall>
2020-09-14 23:41:47 +0100

    Add args parsing

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 #!/usr/bin/env python3
 
 import sys
+import argparse
 import zipfile
 import xml.etree.ElementTree as XMLTree
 
 from xmlmarkupparser import literals, parse
 from xmlwriter import SimpleXmlWriter
 
+def make_argparser():
+    parser = argparse.ArgumentParser(description="Extract xml-markup from docx into xml")
+    parser.add_argument('input', help='input path for docx file')
+    parser.add_argument('--output', help='output path for xml file', default='out.xml')
+    parser.add_argument('--style', help='style name of xml-markup in docx', default='xml-markup')
+    return parser
+
+
 def main():
-    word_file_path = sys.argv[1]
-    with zipfile.ZipFile(word_file_path, 'r') as zip_file:
+    parser = make_argparser()
+    args = parser.parse_args(sys.argv[1:])
+
+    with zipfile.ZipFile(args.input, 'r') as zip_file:
         #print('\n'.join(zip_file.namelist()))
         #doc = zip_file.read('word/document.xml')
         zip_file.extract('word/document.xml')
@@ -18,15 +29,11 @@ def main():
     root = tree.getroot()
 
     ns = {'w':'http://schemas.openxmlformats.org/wordprocessingml/2006/main'}
-    style = 'xml-markup'
-    #for lit in literals(root, ns, 'xml-markup'):
-    #    print(lit)
 
-    tokens = parse(root, ns, style)
+    tokens = parse(root, ns, args.style)
     with open('out.xml', 'w') as f:
         writer = SimpleXmlWriter(f)
         for tok in tokens:
-            print(tok)
             writer.write(tok)
 
 
